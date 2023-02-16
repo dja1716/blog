be803a91fff9e83c174f1e50188803064797ddbe
apple <apple@appleui-MacBookPro.local>
2023-02-16 16:31:32 +0900

    a bit of responsiveness test

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,32 @@ import { HelmetProvider } from "react-helmet-async";
 import { AppContainer, GlobalStyles } from "./styles/styles";
 import { Route, Routes } from "react-router-dom";
 import { routes } from "./routes";
-import Home from "./pages/Home";
-import NotFoundPage from "./pages/NotFoundPage";
+import Home from "./pages/PC/Home";
+import NotFoundPage from "./pages/PC/NotFoundPage";
+import { useMediaQuery } from "react-responsive";
+import MHome from "./pages/Mobile/MHome";
+import MNotFoundPage from "./pages/Mobile/MNotFoundPage";
 
 function App() {
+  const isPC = useMediaQuery({
+    query: "(min-width : 1024px)",
+  });
   return (
     <PortalProvider>
       <HelmetProvider>
         <GlobalStyles />
         <AppContainer>
-          <Routes>
-            <Route path={routes.root} element={<Home />} />
-            <Route path={"*"} element={<NotFoundPage />} />
-          </Routes>
+          {isPC ? (
+            <Routes>
+              <Route path={routes.root} element={<Home />} />
+              <Route path={"*"} element={<NotFoundPage />} />
+            </Routes>
+          ) : (
+            <Routes>
+              <Route path={routes.root} element={<MHome />} />
+              <Route path={"*"} element={<MNotFoundPage />} />
+            </Routes>
+          )}
         </AppContainer>
       </HelmetProvider>
     </PortalProvider>
---
+++ src/pages/Mobile/MHome.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+function MHome({}: Props) {
+  return <div>Home mobile</div>;
+}
+
+export default MHome;
