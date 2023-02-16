26780b3594a0ead45876f29597705412216aa7c6
apple <apple@appleui-MacBookPro.local>
2023-02-16 15:31:43 +0900

    prettier fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { PortalProvider } from './providers/PortalProvider';
-import { HelmetProvider } from 'react-helmet-async';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { PortalProvider } from "./providers/PortalProvider";
+import { HelmetProvider } from "react-helmet-async";
 
 function App() {
   return (
-
     <PortalProvider>
       <HelmetProvider>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-
-     </HelmetProvider>
-     </PortalProvider>
-     
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </HelmetProvider>
+    </PortalProvider>
   );
 }
 
--- src/providers/PortalProvider.tsx
+++ src/providers/PortalProvider.tsx
@@ -4,37 +4,42 @@ import { createPortal } from "react-dom";
 export const PortalContext = createContext<HTMLDivElement | null>(null);
 
 interface PortalProviderProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
-export function PortalProvider({children}: PortalProviderProps) {
-    const [portalContainerRef, setPortalContainerRef] = useState<HTMLDivElement | null>(null);
+export function PortalProvider({ children }: PortalProviderProps) {
+  const [portalContainerRef, setPortalContainerRef] =
+    useState<HTMLDivElement | null>(null);
 
-
-    return (
-        <PortalContext.Provider value={portalContainerRef}>
-            {children}
-            <div id="portal-container" ref={(elem) => {
-                if(portalContainerRef !== null || elem === null) {
-                    return
-                }
-                setPortalContainerRef(elem);
-            }} />
-        </PortalContext.Provider>
-    )
+  return (
+    <PortalContext.Provider value={portalContainerRef}>
+      {children}
+      <div
+        id="portal-container"
+        ref={(elem) => {
+          if (portalContainerRef !== null || elem === null) {
+            return;
+          }
+          setPortalContainerRef(elem);
+        }}
+      />
+    </PortalContext.Provider>
+  );
 }
 
 interface PortalConsumerProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
-export function PortalConsumer({children} : PortalConsumerProps) {
-    return (<PortalContext.Consumer>
-        {(portalContainerRef => {
-            if(portalContainerRef === null) {
-                return null;
-            }
-            return createPortal(children, portalContainerRef)
-        })}
-    </PortalContext.Consumer>)
+export function PortalConsumer({ children }: PortalConsumerProps) {
+  return (
+    <PortalContext.Consumer>
+      {(portalContainerRef) => {
+        if (portalContainerRef === null) {
+          return null;
+        }
+        return createPortal(children, portalContainerRef);
+      }}
+    </PortalContext.Consumer>
+  );
 }
