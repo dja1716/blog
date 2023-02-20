cedaeb0f88a74a1163e6f001da3d4f6aa6068acf
dja1716 <hdum@uwaterloo.ca>
2023-02-20 14:14:22 +0900

    git testing

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,6 +2,8 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
+// git testing
+
 function NavBar() {
   return (
     <Navbar expand="lg">
