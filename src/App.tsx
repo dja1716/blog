575c8975b6ecebd2fa2e9c27d88c0cb15bfc19d3
dja1716 <hdum@uwaterloo.ca>
2023-02-21 14:37:03 +0900

    edit: App

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import NavBar from "./components/NavBar";
 import Banner from "./components/Banner";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   const isPC = useMediaQuery({
