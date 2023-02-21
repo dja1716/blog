6833e56f9e56ba445418806e92de68929538c7cd
dja1716 <hdum@uwaterloo.ca>
2023-02-21 09:52:27 +0900

    new: ProjectCard setup

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {
+  title: string;
+  description: string;
+  imgUrl: string;
+};
+
+export default function ProjectCard({ title, description, imgUrl }: Props) {
+  return <div>ProjectCard</div>;
+}
