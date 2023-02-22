c4a239256f4a35cbe55ab5cacaa293307a2dd252
dja1716 <hdum@uwaterloo.ca>
2023-02-22 16:33:28 +0900

    edit: Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Col, Container } from "react-bootstrap";
 import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
 
 type Props = {};
 
@@ -11,7 +14,20 @@ export default function Footer({}: Props) {
         <Col sm={6}>
           <img src={logo} alt="Logo" />
         </Col>
-        <Col sm={6} className="text-center text-sm-end"></Col>
+        <Col sm={6} className="text-center text-sm-end">
+          <div className="social-icon">
+            <a href="">
+              <img src={navIcon1} />
+            </a>
+            <a href="">
+              <img src={navIcon2} />
+            </a>
+            <a href="">
+              <img src={navIcon3} />
+            </a>
+          </div>
+          <p>CopyRight 2023. All Right Reserved by Hyeon Um</p>
+        </Col>
       </Container>
     </footer>
   );
