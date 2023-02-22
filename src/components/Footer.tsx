9ab85dcfe2bbf5f4828e05c96ccb7e890433b187
dja1716 <hdum@uwaterloo.ca>
2023-02-22 16:29:01 +0900

    edit: Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Col, Container } from "react-bootstrap";
+import logo from "../assets/img/logo.svg";
 
 type Props = {};
 
 export default function Footer({}: Props) {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <Container className="align-item-center">
+        <Col sm={6}>
+          <img src={logo} alt="Logo" />
+        </Col>
+        <Col sm={6} className="text-center text-sm-end"></Col>
+      </Container>
+    </footer>
+  );
 }
